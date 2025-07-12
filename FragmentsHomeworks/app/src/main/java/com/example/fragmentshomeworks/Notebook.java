082670d9c35af05323e83d8247b17102bc332347
Artem <twistersister987@gmail,com>
2025-07-12 10:58:41 +0300

    change

--- FragmentsHomeworks/app/src/main/java/com/example/fragmentshomeworks/Notebook.java
+++ FragmentsHomeworks/app/src/main/java/com/example/fragmentshomeworks/Notebook.java
@@ -7,12 +7,10 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,9 +19,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
-
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
